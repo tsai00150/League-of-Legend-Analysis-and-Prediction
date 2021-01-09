9f051ab4aa3948b3ff4e3721b8d3d02ad64f0fb1
CCMinyi <63200665+CCMinyi@users.noreply.github.com>
2021-01-09 22:09:18 +0800

    Add files via upload
    for shiny

--- code/KDA.R
+++ code/KDA.R
@@ -18,7 +18,7 @@ matches <- cbind(matches,
 matches_blue_kda<-matches[,c('blue_ad_kad','blue_sup_kad','blue_mid_kad','blue_jungle_kad','blue_top_kad'
                         )]
 #计算相关系数，方法选择pearson
-cor(matches_blue_kda, method="pearson")
+blue_kad_cor <- data.frame(cor(matches_blue_kda, method="pearson"))
 #画出相关性矩阵图，标题：相关性矩阵图
 pairs(matches_blue_kda,spread = F,lty.smooth=2,main='blue_kda_correlation')
 
@@ -27,40 +27,12 @@ matches_purple_kda<-matches[,c('purple_ad_kad','purple_sup_kad','purple_mid_kad'
 )]
 
 #计算相关系数，方法选择pearson
-cor(matches_purple_kda, method="pearson")
+purple_kad_cor <- data.frame(cor(matches_purple_kda, method="pearson"))
 #画出相关性矩阵图，标题：相关性矩阵图
 pairs(matches_purple_kda,spread = F,lty.smooth=2,main='purple_kda_correlation')
 
 #我用summary看 決定用 2 4 6區分 分成四個區間 發現效果不好 改成大於6的視為6
 #blue_ad_kad.fix <- ifelse(matches$blue_ad_kad<2,1,ifelse(matches$blue_ad_kad<4,2,ifelse(matches$blue_ad_kad<6,3,4)))
-matches <- cbind(matches,
-                 blue_ad_kad_range = ifelse(matches$blue_ad_kad>6,6,matches$blue_ad_kad),
-                 blue_sup_kad_range = ifelse(matches$blue_sup_kad>6,6,matches$blue_sup_kad),
-                 blue_mid_kad_range = ifelse(matches$blue_mid_kad>6,6,matches$blue_mid_kad),
-                 blue_jungle_kad_range = ifelse(matches$blue_jungle_kad>6,6,matches$blue_jungle_kad),
-                 blue_top_kad_range = ifelse(matches$blue_top_kad>6,6,matches$blue_top_kad),
-                 purple_ad_kad_range = ifelse(matches$purple_ad_kad>6,6,matches$purple_ad_kad),
-                 purple_sup_kad_range = ifelse(matches$purple_sup_kad>6,6,matches$purple_sup_kad),
-                 purple_mid_kad_range = ifelse(matches$purple_mid_kad>6,6,matches$purple_mid_kad),
-                 purple_jungle_kad_range = ifelse(matches$purple_jungle_kad>6,6,matches$purple_jungle_kad),
-                 purple_top_kad_range = ifelse(matches$purple_top_kad>6,6,matches$purple_top_kad)
-)
-
-matches_blue_kda_range<-matches[,c('blue_ad_kad_range','blue_sup_kad_range','blue_mid_kad_range','blue_jungle_kad_range','blue_top_kad_range'
-)]
-#计算相关系数，方法选择pearson
-cor(matches_blue_kda_range, method="pearson")
-#画出相关性矩阵图，标题：相关性矩阵图
-pairs(matches_blue_kda_range,spread = F,lty.smooth=2,main='blue_kda_range_correlation')
-
-#KDA比較(沒用用range) purple
-matches_purple_kda_range<-matches[,c('purple_ad_kad_range','purple_sup_kad_range','purple_mid_kad_range','purple_jungle_kad_range','purple_top_kad_range'
-)]
-
-#计算相关系数，方法选择pearson
-cor(matches_purple_kda_range, method="pearson")
-#画出相关性矩阵图，标题：相关性矩阵图
-pairs(matches_purple_kda_range,spread = F,lty.smooth=2,main='purple_kda_range_correlation')
 
 
 #KDA取ln
@@ -80,7 +52,7 @@ matches <- cbind(matches,
 matches_blue_kda_ln<-matches[,c('blue_ad_kad_ln','blue_sup_kad_ln','blue_mid_kad_ln','blue_jungle_kad_ln','blue_top_kad_ln'
 )]
 #计算相关系数，方法选择pearson
-cor(matches_blue_kda_ln, method="pearson")
+blue_kda_ln_cor <- data.frame(cor(matches_blue_kda_ln, method="pearson"))
 #画出相关性矩阵图，标题：相关性矩阵图
 pairs(matches_blue_kda_range,spread = F,lty.smooth=2,main='blue_kda_ln_correlation')
 
@@ -89,7 +61,7 @@ matches_purple_kda_ln<-matches[,c('purple_ad_kad_ln','purple_sup_kad_ln','purple
 )]
 
 #计算相关系数，方法选择pearson
-cor(matches_purple_kda_ln, method="pearson")
+purple_kda_ln_cor <- data.drame(cor(matches_purple_kda_ln, method="pearson"))
 #画出相关性矩阵图，标题：相关性矩阵图
 pairs(matches_purple_kda_ln,spread = F,lty.smooth=2,main='purple_kda_ln_correlation')
 
